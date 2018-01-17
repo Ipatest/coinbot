6e93bd9225cdf1e87d43a92a6abb6bb1147a63d5
Ipatest <35423613+Ipatest@users.noreply.github.com>
2018-01-17 12:02:56 +0300

    a

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# webhook
+URL = os.environ.get('https://chat3test2.herokuapp.com/')
+PORT = int(os.environ.get('PORT', '5000'))
+
+# telegram
+TOKEN = os.environ.get('432474721:AAEzipq0SAiywuZyRE1-nVhHVVvenHq_Vug')
